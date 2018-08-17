9093b5adf85314cf8b2ca3568199359a86e92950
minung.han <hmu332233@gmail.com>
2018-08-17 06:04:23 +0000

    [Add] contents.html

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+var fs = require('fs');
+
 var LOGIN_URL = "https://portal.jnu.ac.kr/Common/Login/Login.aspx";
 var WRITE_PAGE_URL = "http://www.jnu.ac.kr/WebApp/web/HOM/COM/Board/board.aspx?boardID=10&bbsMode=write&page=1";
 var ID = '';
 var PW = '';
 
-var TITLE = "[신축] 예대 1분거리 신축 원룸 입주자 구합니다";
+var TITLE = "★첫입주★(사진있음) 예대, 생활관 1분거리. 신축 풀옵션 원룸";
 var USER_NAME = '원룸그레이스';
-var CONTENTS = '테스트\n테스트';
+var CONTENTS = fs.read('./contents.html', 'utf8');
 
 console.log('글 작성 시작');
 
@@ -47,17 +49,39 @@ casper.thenOpen(WRITE_PAGE_URL, function() {
 });
 
 casper.viewport(1400, 800);
-// //스크린샷 찍고 페이지 저장하기
+// 글 작성 시작
 casper.then(function(){
     console.log("3초 후에 글이 작성됩니다.");
     this.evaluate(function(title, userName, contents){
         document.getElementById("ctl00_ctl00_ContentPlaceHolder1_PageContent_ctl00_txt_boardTitle").value = title;
         document.getElementById('ctl00_ctl00_ContentPlaceHolder1_PageContent_ctl00_txt_boardWriter').value = userName;
-        var iFrame = document.querySelector('.cke_wysiwyg_frame');
-        iFrame.contentWindow.document.body.innerText = contents;
+        // html 없이 올리는 화면
+        // var iFrame = document.querySelector('.cke_wysiwyg_frame');
+        // iFrame.contentWindow.document.body.innerText = contents;
     }, {
       title: TITLE,
       userName: USER_NAME,
+      contents: CONTENTS
+    });
+});
+
+casper.then(function(){ this.wait(1000, function () {} ) });
+
+casper.then(function(){
+    console.log("소스코드 모드 클릭");
+    this.evaluate(function(){
+        document.getElementById('cke_18').click();
+    });
+});
+
+casper.then(function(){ this.wait(1000, function () {} ) });
+
+casper.then(function(){
+    console.log("글 내용 삽입");
+    this.evaluate(function(contents){
+        // 소스화면
+        document.querySelector('#cke_1_contents > textarea').value = contents;
+    }, {
       contents: CONTENTS
     });
 });
