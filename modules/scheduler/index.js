5763c508b84aa1d36d535a4fcbbd787421d48278
minung.han <hmu332233@gmail.com>
2018-08-20 00:00:22 +0900

    [Modify] 포스팅시 LineNotify 보내도록

--- modules/scheduler/index.js
+++ modules/scheduler/index.js
@@ -1,19 +1,20 @@
 const cron = require('node-cron');
 const JnuPoster = require('../jnuPoster/index.js');
+const NotifySender = require('../NotifySender/index.js');
 
 const jnuPoster = new JnuPoster();
 const execPost = (time) => {
-  console.log(`${time} 포스팅 예약 등록되었습니다.`);
+  NotifySender.send(`${time} 포스팅 예약 등록되었습니다.`)
   cron.schedule(time, () => {
-    console.log(`${time} 포스팅 시작합니다.`);
+    NotifySender.send(`${time} 포스팅 시작합니다.`)
     jnuPoster.post();
   });
 }
 
 const postTimes = [
-  '0 10 * * *',
-  '0 15 * * *',
-  '0 22 * * *'
+  '0 1 * * *',
+  '0 6 * * *',
+  '0 13 * * *'
 ];
 
 postTimes.forEach(time => {
